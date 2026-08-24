3ebffec7e60a4a740677717240bbe07dd888d2a7
MaryLove <marylovenjampou@gmail.com>
2026-08-24 05:13:30 +0100

    the App component modified by adding the browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,43 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Order } from "./pages/Order";
 import { TheHotel } from "./pages/TheHotel"
 import HomePage from "./pages/Home"
 import { Room } from "./pages/Room"
-import { Services } from "./pages/Services"
+import { Services } from "./pages/About"
 import { ContactPage } from "./pages/ContactPage"
 import { Map } from "./pages/map"
+import { Testimonial } from "./pages/Testimonial"
 function App() {
   return (
     <div className="h-screen ">
+      <BrowserRouter>
+      <Routes>
 
+        <Route
+          path="/"
+          element={
+            <>
+              <HomePage />
+              <TheHotel />
+              <Room />
+              <Services />
+              <Map />
+              <Testimonial />
+              <ContactPage />
+            </>
+          }
+        />
 
-     <HomePage/>
-     <TheHotel/>
-     <Room/>
-     <Services/>
-     <Map/>
+        <Route path="/order" element={<Order />} />
 
-   <ContactPage/>
+      </Routes>
+    </BrowserRouter>
+    
+    
+     
+    
+
+  
 
     </div>
   )
