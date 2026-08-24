02f387eee26bb55872f85631d434d0b08eb8b83b
MaryLove <marylovenjampou@gmail.com>
2026-08-24 04:53:51 +0100

    i finished my room pages which lets visitors view the rooms and also the aboutus page

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -4,20 +4,19 @@ import  room3 from '../assets/images/room5lafalaise.webp';
 import  room4 from '../assets/images/FalaiseRooms/falaiseroom2.jpg';
 import  room5 from '../assets/images/FalaiseRooms/LaFalaiseroom4.jpg';
 
-import  room6 from '../assets/images/FalaiseRooms/LaFalaiseroom6.jpg';
+import  room6 from '../assets/images/room4lafalaise.webp';
 
 import  room7 from '../assets/images/FalaiseRooms/LaFalaiseroom10.jpg';
 
-import  room10 from '../assets/images/room6lafalaise.webp';
+import  room10 from '../assets/images/room7lafalaise.webp';
 
 export function Room(){
   return(
-    <div className="h-150 w-full bg-[#E8DED4]  p-8 ">
-
-     <div className='mb-10 ml-15'>
+    <div id='room' className="h-150 w-full bg-[#E8DED4]  p-8 ">
+<div className='mb-10 ml-15'>
        <div className='text-[#3B2720] text-4xl mb-5 font-arial font-bold'>
-        Our rooms
-      </div>
+       Our rooms
+    </div>
       <div className='text-[#6B4F43]'>
         <p>All our rooms have one thing in common,</p>
         <p>they are designed to ensure that your stay with us is as relaxing and comfortable as possible</p>
@@ -74,7 +73,7 @@ export function Room(){
  <div className='relative flex justify-start items-end'>
 <img src={room7} alt="" className=' h-40 absolute rounded-lg'/>
 <div className='absolute text-white mb-3 ml-2'>
-<a href="" className=' text-[20px]'>Deluxe</a>
+<a href="" className='text-[20px]'>Deluxe</a>
 </div>
  </div>
 
